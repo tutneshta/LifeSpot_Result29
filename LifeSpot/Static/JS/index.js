dbf75ad24bc3d884b5a7f90b2011461025472ec4
tutneshta <tutneshta@yandex.ru>
2023-02-05 18:49:33 +0300

    29_5_5

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,42 +1,35 @@
-﻿function handleSession() {
-    // создадим объект Map для хранения сессии
-    let session = new Map();
+﻿let session = new Map();
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
     // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent);
+    session.set("userAgent", window.navigator.userAgent)
+}
 
-    // Запросим возраст пользователя и тоже сохраним
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"));
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
-    // Проверка на возраст и сохранение сессии
     if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate);
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com";
+        window.location.href = "http://www.google.com"
     }
-
-    // Теперь мы возвращаем объект сессии
-    return session;
 }
 
-let sessionLog = function logSession(session) {
-    // Вывод в консоль
+let sessionLog = function logSession() {
     for (let result of session) {
-        console.log(result);
+        console.log(result)
     }
 }
 
-function filterContent(inputParseFunction) { // в этот параметр будет передана ФУНКЦИЯ парсинга пользовательского ввода
-
+function filterContent() {
     let elements = document.getElementsByClassName('video-container');
 
     for (let i = 0; i <= elements.length; i++) {
         let videoText = elements[i].querySelector(".video-title").innerText;
-        if (!videoText.toLowerCase().includes(inputParseFunction() /*Переданная функция вызвана*/.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */.toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
