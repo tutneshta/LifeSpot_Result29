d93f475b915d9d1d94e078c819f747e86217259f
tutneshta <tutneshta@yandex.ru>
2023-02-05 15:56:18 +0300

    29_4_4_1

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -30,23 +30,15 @@ let sessionLog = function logSession(session) {
     }
 }
 
-function filterContent(userInput) {
+function filterContent(inputParseFunction) { // в этот параметр будет передана ФУНКЦИЯ парсинга пользовательского ввода
 
-    // Сохраняем текст пользовательского запроса.
-   /* let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();*/
-    // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
 
-    // Пробегаемся по контейнерам
     for (let i = 0; i <= elements.length; i++) {
-        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
         let videoText = elements[i].querySelector(".video-title").innerText;
-        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
-            // Скрываем неподходящие
+        if (!videoText.toLowerCase().includes(inputParseFunction() /*Переданная функция вызвана*/.toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
-            // Показываем подходящие
             elements[i].style.display = 'inline-block';
         }
     }
